b19aa1102ba308e3f79cf1d6c3d5adc05e09e6fb
Partha Mehta <parthamehta10@gmail.com>
2023-04-20 03:29:54 -0400

    Add files via upload

--- colaberrygluejobcleandataingestionweatherandcropdata.py
+++ colaberrygluejobcleandataingestionweatherandcropdata.py
@@ -1,5 +1,3 @@
-# This is a file/code file placed in the script section of my Glue Job for deployment purpose or for entire architecture of this project to run on AWS Cloud.
-
 # Cleaning, Transforming and Ingesting Crop Data Right Now and Writing this Data to PostgreSQL on RDS
 
 # Import the required libraries and set up the AWS Glue context:
@@ -176,7 +174,7 @@ def main():
             year INT NOT NULL,
             corn_yield DOUBLE NOT NULL,
             created_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            updated_timestamp TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+            updated_timestamp TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
             ) ENGINE=InnoDB;
             """
             # create a cursor object
@@ -446,7 +444,7 @@ def main():
             precipitation DOUBLE NOT NULL,
             station_id VARCHAR(30) NOT NULL,
             created_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            updated_timestamp TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+            updated_timestamp TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
             ) ENGINE=InnoDB;
             """
             # create a cursor object
