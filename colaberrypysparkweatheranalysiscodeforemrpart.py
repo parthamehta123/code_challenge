cde64cda7f68c608364be1951f7678bb8abfd64f
Partha Mehta <parthamehta10@gmail.com>
2023-04-26 20:37:42 -0400

    Add files via upload

--- colaberrypysparkweatheranalysiscodeforemrpart.py
+++ colaberrypysparkweatheranalysiscodeforemrpart.py
@@ -1,3 +1,4 @@
+import pymysql
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql import DataFrameWriter
@@ -18,7 +19,7 @@ spark = SparkSession.builder \
 aurora_url = "jdbc:mysql://colaberrydb.ctkwfn0vycpa.us-east-2.rds.amazonaws.com:3306/colaberryrdsdb"
 properties = {
     "user": "admin",
-    "password": "Password123",
+    "password": "TelevSudha123",
     "driver": "com.mysql.jdbc.Driver"
 }
 
@@ -34,35 +35,71 @@ weather_data = spark.read.format("jdbc") \
 weather_data.show()
 
 # Define the schema for the Statistics table
-schema = StructType([
-    StructField("station_id", StringType(), False),
-    StructField("year", IntegerType(), False),
-    StructField("avg_max_temp", DoubleType()),
-    StructField("avg_min_temp", DoubleType()),
-    StructField("total_precipitation", DoubleType()),
-    StructField("created_timestamp", TimestampType(), False),
-    StructField("updated_timestamp", TimestampType(), False),
-])
-
-# Define the table schema
-table_schema = """
-    CREATE TABLE weather_statistics (
-        station_id VARCHAR(30) NOT NULL,
-        year INT NOT NULL,
-        avg_max_temp DOUBLE,
-        avg_min_temp DOUBLE,
-        total_precipitation DOUBLE,
-        created_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        updated_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        PRIMARY KEY (year, station_id)
-    ) ENGINE=InnoDB;
-"""
+# schema = StructType([
+#     StructField("station_id", StringType(), False),
+#     StructField("year", IntegerType(), False),
+#     StructField("avg_max_temp", DoubleType()),
+#     StructField("avg_min_temp", DoubleType()),
+#     StructField("total_precipitation", DoubleType()),
+#     StructField("created_timestamp", TimestampType(), False),
+#     StructField("updated_timestamp", TimestampType(), False),
+# ])
 
 # Create the empty table
 spark.read.format("jdbc").options(url=aurora_url, driver=properties["driver"], user=properties["user"],
                                   password=properties["password"],
                                   dbtable="(SELECT 1) tmp").load().createOrReplaceTempView("tmp_table")
-spark.sql(table_schema)
+
+# create connection to the MySQL database on AWS RDS
+connection = pymysql.connect(
+    host='colaberrydb.ctkwfn0vycpa.us-east-2.rds.amazonaws.com',
+    user='admin',
+    password='TelevSudha123',
+    db='colaberryrdsdb',
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+# list all tables in the database
+with connection.cursor() as cursor:
+    cursor.execute("SHOW TABLES")
+    table_list = [row['Tables_in_colaberryrdsdb'] for row in cursor.fetchall()]
+
+print("table_list :: ")
+print(table_list)
+
+table_name = 'weather_statistics'
+
+# check if the table exists
+if table_name not in table_list:
+
+    # create the table if it doesn't exist along with its schema
+    try:
+        # Define the SQL query to create the table
+        create_table_query = """
+            CREATE TABLE weather_statistics (
+                        station_id VARCHAR(30) NOT NULL,
+                        year INT NOT NULL,
+                        avg_max_temp DOUBLE,
+                        avg_min_temp DOUBLE,
+                        total_precipitation DOUBLE,
+                        created_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                        updated_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                        PRIMARY KEY (year, station_id)
+                    ) ENGINE=InnoDB;
+            """
+        # create a cursor object
+        with connection.cursor() as cursor:
+            # execute the SQL query to create the table
+            cursor.execute(create_table_query)
+            print("Table created successfully")
+        # commit the transaction
+        connection.commit()
+    except Exception as e:
+        print("Error creating table :: ")
+        print(str(e))
+
+# spark.sql(table_schema)
 # # Create a DataFrame with the specified schema
 # empty_df = spark.createDataFrame([], schema)
 #
@@ -82,8 +119,13 @@ statistics = weather_data.filter(
 
 statistics.write.jdbc(url=aurora_url, table="weather_statistics", mode="overwrite", properties=properties)
 
-statistics.coalesce(1).write.format("csv").option("header", "true").save("s3a://colaberrycodechallenges3"
-                                                                         "/emrsparkcodeoutputlocation/")
+statistics \
+    .coalesce(1)\
+    .write\
+    .mode("overwrite")\
+    .format("csv")\
+    .option("header", "true")\
+    .save("s3a://colaberrycodechallenges3/emrsparkcodeoutputlocation/")
 
 print("Weather Statistics Data Calculated and Stats are Written to the MySQL DB")
 statistics.show()
